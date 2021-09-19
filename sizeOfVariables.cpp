4623b1d254397e027cab50f8486614c4fb5d2157
AppannaBabu <70013688+AppannaBabu@users.noreply.github.com>
2021-09-19 06:33:55 +0530

    Create sizeOfVariables.cpp
    prints size of different variables

---
+++ sizeOfVariables.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int a;
+  char b;
+  float c;
+  double d;
+
+  cout <<"Size of int: "<<sizeof(a)<<" byte"; 
+  cout <<"\nSize of char: "<<sizeof(b)<<" byte";
+  cout <<"\nSize of float: "<<sizeof(c)<<" byte";
+  cout <<"\nSize of double: "<<sizeof(d)<<" byte";
+}
